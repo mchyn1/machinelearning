1e1288c38b4a5996128da3831907db55b9e8615e
mchyn1 <mchyn1@jhu.edu>
2016-05-06 23:20:13 -0400

    errrrr
    ew

--- FinalProj.m
+++ FinalProj.m
@@ -21,33 +21,47 @@ for n = 1:num %number of individuals/subspaces
     x = reshape(x,[a*b,c]);
     X = [X x];
 end
-
+data = cell(8,1);
 tau_range = [1*10^-7 1*10^-6 1*10^-5 0.0001 0.001 0.01 0.1 1 10 100];
 %% Adding uniform noise to (20:20:80)% of pixels
 % add a loop to see if using Gaussian Noise makes a difference?
+
 percent = .2:.2:.8;
 for i = 1:length(percent)
     X_noise=X;
     ind = randperm(a*b);
-    noise = unifrnd(-256,256,[round(a*b*percent(i)),1]);
+    noise = unifrnd(-60,60,[round(a*b*percent(i)),1]);
     X_noise(ind(1:round(a*b*percent(i))),:)=bsxfun(@plus,X_noise(ind(1:round(a*b*percent(i))),:),noise);
+    data{i} = X_noise;
 end
 
 %% Corrupting (20:20:80)% of pixels
 % 1) by removing them
 % 2) by replacing with outliers of some type
+for i = 1:length(percent)
+    X_corrupt=X;
+    ind = randperm(a*b);
+    noise = unifrnd(-256,256,[round(a*b*percent(i)),1]);
+    X_corrupt(ind(1:round(a*b*percent(i))),:)=bsxfun(@plus,X_corrupt(ind(1:round(a*b*percent(i))),:),noise);
+    data{i+4} = X_corrupt;
+end
 
 
 %% LRSC with noise
 
 %convex
-[C, tau, error] = find_tau(X_noise,tau_range,'noise convex',num,c);
+[C, tau, error, X_new] = find_tau(X_noise,tau_range,'noise convex',num,c);
 tau
 figure;
 scatter(1:n*c,C);
+for i = 1:c*n
+    figure;
+    x = reshape(X(:,i),a,b);
+    subplot(3,c,i);imshow(x,'DisplayRange',[]);
+end
 
 %non-convex
-[C, tau, error] = find_tau(X_noise,tau_range,'noise nonconvex',num,c);
+[C, tau, error, X_new] = find_tau(X_noise,tau_range,'noise nonconvex',num,c);
 tau
 figure;
 scatter(1:n*c,C);
@@ -56,19 +70,19 @@ scatter(1:n*c,C);
 
 %% LRSC with corruptions
 %convex
-[C, tau, error] = find_tau(X_noise,tau_range,'corrupt convex',num,c);
+[C, tau, error, X_new] = find_tau(X_noise,tau_range,'corrupt convex',num,c);
 tau
 figure;
 scatter(1:n*c,C);
 
 %non-convex
-[C, tau, error] = find_tau(X_noise,tau_range,'corrupt nonconvex',num,c);
+[C, tau, error, X_new] = find_tau(X_noise,tau_range,'corrupt nonconvex',num,c);
 tau
 figure;
 scatter(1:n*c,C);
 
 %% LRSC with Uncorrupted Entries
-[C, tau, error] = find_tau(X_noise,tau_range,'uncorrupt',num,c);
+[C, tau, error, X_new] = find_tau(X_noise,tau_range,'uncorrupt',num,c);
 tau
 figure;
 scatter(1:n*c,C);
